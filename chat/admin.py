6d8aff81e4420bbf3ab6730fe8d2497e2646a204
Jephtha <jephtroland832@gmail.com>
2025-08-16 13:58:04 +0100

    New

--- chat/admin.py
+++ chat/admin.py
@@ -3,4 +3,9 @@ from .models import Report
 
 admin.site.register(Report)
 
+
+# Change Admin site headers and titles
+admin.site.site_header = "Ulcer Expert System Administration"
+admin.site.site_title = "Ulcer Expert System Admin Portal"
+admin.site.index_title = "Welcome to Ulcer Expert System Dashboard"
 # Register your models here.
